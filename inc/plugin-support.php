de84a46a9b434173de3ad1e0a5e25006738faf54
Marko Heijnen <info@markoheijnen.com>
2012-07-02 18:37:57 +0200

    Added ugly fix for the plugin: Advanced custom fields

--- inc/plugin-support.php
+++ inc/plugin-support.php
@@ -12,5 +12,32 @@ class Tabify_Edit_Screen_Plugin_Support {
 			wpcf_admin_post_page_load_hook();
 			unset( $_GET['post_type'] );
 		}
+
+		global $acf;
+		if ( is_object( $acf ) ) {
+			$acfs_objects = $acf->get_field_groups();
+
+			if( $acfs_objects )
+			{
+				foreach( $acfs_objects as $acf_object )
+				{
+					$metabox_ids = $acf->get_input_metabox_ids( array(), false );
+
+					// hide / show
+					$show = in_array( $acf_object['id'], $metabox_ids ) ? "true" : "false";
+
+					// add meta box
+					add_meta_box(
+						'acf_' . $acf_object['id'], 
+						$acf_object['title'], 
+						array( $acf_object, 'meta_box_input' ), 
+						$posttype, 
+						$acf_object['options']['position'], 
+						'high', 
+						array( 'fields' => $acf_object['fields'], 'options' => $acf_object['options'], 'show' => $show )
+					);
+				}
+			}
+		}
 	}
 }
