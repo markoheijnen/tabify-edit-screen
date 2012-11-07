5ca8086bfed4cf92d97c6caf3317490f33348223
Marko Heijnen <info@markoheijnen.com>
2012-11-07 13:55:27 +0100

    Removed Advanced custom field support since they did it for us :) Major thanks for that

--- inc/plugin-support.php
+++ inc/plugin-support.php
@@ -3,7 +3,6 @@
 class Tabify_Edit_Screen_Plugin_Support {
 	function __construct() {
 		add_action( 'tabify_add_meta_boxes', array( &$this, 'types' ) );
-		add_action( 'tabify_add_meta_boxes', array( &$this, 'acf' ) );
 		add_action( 'tabify_add_meta_boxes', array( &$this, 'wpseo' ) );
 		add_action( 'tabify_add_meta_boxes', array( &$this, 'members' ) );
 	}
@@ -20,43 +19,6 @@ class Tabify_Edit_Screen_Plugin_Support {
 			$_GET['post_type'] = $posttype;
 			wpcf_admin_post_page_load_hook();
 			unset( $_GET['post_type'] );
-		}
-	}
-
-	/**
-	 * Load widgets created by ACF
-	 *
-	 * @param string $posttype The posttype the metaboxes should be loaded from
-	 * 
-	 * @since 0.4
-	 */
-	function acf() {
-		global $acf;
-
-		if ( is_object( $acf ) ) {
-			$acfs_objects = $acf->get_field_groups();
-
-			if( $acfs_objects )
-			{
-				foreach( $acfs_objects as $acf_object )
-				{
-					$metabox_ids = $acf->get_input_metabox_ids( array(), false );
-
-					// hide / show
-					$show = in_array( $acf_object['id'], $metabox_ids ) ? "true" : "false";
-
-					// add meta box
-					add_meta_box(
-						'acf_' . $acf_object['id'], 
-						$acf_object['title'], 
-						array( $acf_object, 'meta_box_input' ), 
-						$posttype, 
-						$acf_object['options']['position'], 
-						'high', 
-						array( 'fields' => $acf_object['fields'], 'options' => $acf_object['options'], 'show' => $show )
-					);
-				}
-			}
 		}
 	}
 
