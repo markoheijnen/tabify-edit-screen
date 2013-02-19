1c79b7ef21c8e4605495ec0b3e334e649e6d8fdb
Marko Heijnen <info@markoheijnen.com>
2013-02-19 20:23:54 +0100

    Added first real test. So many need to follow. See #18

---
+++ tests/test_core.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Tests to test that that testing framework is testing tests. Meta, huh?
+ * @package wordpress-plugins-tests
+ */
+class Tabify_Tests_Core extends WP_UnitTestCase {
+
+	function test_Tabify_Edit_Screen__redirect_add_current_tab() {
+		global $tabify_edit_screen;
+
+		$this->assertEquals( $tabify_edit_screen->redirect_add_current_tab( 'my-location', 1 ), 'my-location' );
+	}
+
+	function test_Tabify_Edit_Screen__show_tabs() {
+		global $tabify_edit_screen;
+
+		$this->assertFalse( $tabify_edit_screen->show_tabs() );
+	}
+}
