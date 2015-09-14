12c312405f8458847ef91db724543d8e396dbacd
Marko Heijnen <info@markoheijnen.com>
2015-09-14 21:32:26 +0200

    More cleanup in Plugin header

--- tabify-edit-screen.php
+++ tabify-edit-screen.php
@@ -1,15 +1,33 @@
 <?php
 /*
 Plugin Name: Tabify edit screen
-Plugin URI: http://rocksta.rs/plugin/tabify-edit-screen
 Description: Enables tabs in the edit screen and manage them from the back-end
+Version: 0.8.3
+
+Plugin URI: http://rocksta.rs/plugin/tabify-edit-screen
 
 Author: Marko Heijnen
 Author URI: https://markoheijnen.com
 Donate link: https://markoheijnen.com/donate
 
 Text Domain: tabify-edit-screen
-Version: 0.8.3
+Domain Path: /languages
+*/
+
+/*  Copyright 2013-2014 Tabify Edit Screen  (email : info@markoheijnen.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as 
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 class Tabify_Edit_Screen {
