e3877f98b76395e0dd6a67f1202093a9e1c411fe
Marko Heijnen <info@markoheijnen.com>
2015-11-13 01:11:47 +0100

    Update version to 0.9.0

--- tabify-edit-screen.php
+++ tabify-edit-screen.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: Tabify edit screen
 	Description: Enables tabs in the edit screen and manage them from the back-end
-	Version: 0.9.0-beta
+	Version: 0.9.0
 
 	Plugin URI: http://rocksta.rs/plugin/tabify-edit-screen
 
@@ -31,7 +31,7 @@
 */
 
 class Tabify_Edit_Screen {
-	public  $version = '0.9.0-beta';
+	public  $version = '0.9.0';
 	public  $admin;
 	private $editscreen_tabs;
 	private $tab_location = 'default';
