bb04f61494e9a0a787ecdcce12d39216afc92d03
Marko Heijnen <info@markoheijnen.com>
2016-02-22 13:16:00 +0100

    Fix adding missing tab for all post types. See #50

--- features/detection/detection.js
+++ features/detection/detection.js
@@ -14,7 +14,7 @@ jQuery(function($) {
 		$.getJSON( tabify_detection[ posttype ], function( data ) {
 			$.each( data, function( key, metabox ) {
 				if ( ! document.getElementById( posttype + '-' + key ) ) {
-					var tab     = $('.tabify_tab', '.tabifybox-post').last();
+					var tab     = $('.tabify_tab', '.tabifybox-' + posttype).last();
 					var counter = $( '.tabifybox-' + posttype + ' .tabify_control' ).children().length - 1;
 
 					$('ul', tab).append('<li id="' + posttype + '-' + key + '"><div class="menu-item-bar"><div class="menu-item-handle"><span class="item-title">' + metabox.title + '</span><input type="hidden" name="tabify[posttypes][' + posttype + '][tabs][' + counter + '][items][]" value="' + key + '"></div></div></li>');
